81c1814d7f0754904cb6a07d2ee854e1459509cd
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-11-22 03:14:42 -0300

    resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
+  username: string = "";
+
+  toggleButton() {
+    if (this.username) {
+      return false;
+    }
+
+    return true;
+  }
+
+  clearUsername() {
+    this.username = "";
+  }
 }
